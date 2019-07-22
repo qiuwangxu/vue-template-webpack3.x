6df84bde835d93c1b27fbb05c2d5595c29549674
qiuzhi199434 <qiuzhi199434@users.noreply.github.com>
2019-07-22 17:15:34 +0800

    Update api-path.js

--- src/api/api-path.js
+++ src/api/api-path.js
@@ -1,1 +1,9 @@
 const mockIp = process.env.NODE_ENV === 'test' ? 'http://localhost:8080' : '' // 此处的ip主要提供为单元测试时候用可以为任意ip
+const server ='/matchName'
+
+const apiPath = {
+  login: `${server}/login`,
+  getLucyData: `${mockIp}${server}/getLucyData`
+}
+
+export default apiPath
